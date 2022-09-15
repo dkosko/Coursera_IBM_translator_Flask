5ce03869687db8c340bb25bb7967847c604f9984
dkasko <dimasikeagle@gmail.com>
2022-09-15 14:38:41 +0200

    compiled tests.py

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -5,17 +5,27 @@ from translator import *
 class TestEnglishToFrench(unittest.TestCase):
     def test_none(self):
         self.assertIsNone(french_to_english(None))
+        self.assertIsNone(french_to_english(''))
+        self.assertIsNone(french_to_english(" "))
 
     def test_hello(self):
         self.assertEqual(french_to_english('Bonjour'), 'Hello')
+
+    def test_sentence(self):
+        self.assertEqual(french_to_english('Phrase simple pour tester'), 'Simple sentence for testing')
 
 
 class TestFrenchToEnglish(unittest.TestCase):
     def test_none(self):
         self.assertIsNone(english_to_french(None))
+        self.assertIsNone(english_to_french(' '))
+        self.assertIsNone(english_to_french(""))
 
     def test_hello(self):
         self.assertEqual(english_to_french('Hello'), 'Bonjour')
+
+    def test_sentence(self):
+        self.assertEqual(english_to_french('Simple sentence for testing'), 'Phrase simple pour le test')
 
 
 if __name__ == '__main__':
